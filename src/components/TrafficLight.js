3d57708ed8906954ea0c65642f3c692b4d2ee1ec
thegioicuabao <vickyvuvo@gmail.com>
2019-11-06 23:17:47 +0700

    Traffic-Light

---
+++ src/components/TrafficLight.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import './TrafficLight.css';
+import classNames from 'classnames';
+
+const RED = 0;
+const ORANGE = 1;
+const GREEN = 2;
+class TrafficLight extends Component{
+  constructor() {
+    super();
+    this.currentColor = RED;
+  }
+  getNextColor(color){
+    switch(color){
+      case RED:
+        return ORANGE;
+      case ORANGE:
+        return GREEN;
+      case GREEN:
+        return RED;
+    }
+  }
+  render(){
+    return <div className="TrafficLight">
+      <div className={classNames('bulb', 'red', {
+        'active': this.currentColor === RED
+      })} /> 
+      <div className={classNames('bulb', 'orange', {
+        'active': this.currentColor === ORANGE
+      })} /> 
+      <div className={classNames('bulb', 'green', {
+        'active': this.currentColor === GREEN
+      })} /> 
+     </div>
+  } 
+}
+export default TrafficLight
